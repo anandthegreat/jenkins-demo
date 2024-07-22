9bbacc948522362021966d94067c9f5df4b2d388
Anand <anand17218@iiitd.ac.in>
2024-07-22 13:13:03 +0530

    fix test

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -5,7 +5,7 @@ from src.calc_demo import *
 def test_calc():
     a = 10
     b = 0
-    with pytest.raises(ValueError):
+    with pytest.raises(ZeroDivisionError):
         div(a,b)
 
     c = add(a,b)
